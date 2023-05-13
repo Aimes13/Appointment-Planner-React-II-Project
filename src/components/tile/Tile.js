7a5b912d1a90c3d868187ec44df84d0a3912b660
Anne-Marie <108076177+Aimes13@users.noreply.github.com>
2023-05-14 01:31:12 +0800

    Update Tile.js

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { TileList } from "../tileList/TileList";
 
 export const Tile = ( {name, description} ) => {
   return (
     <div className="tile-container">
-      {
-        (Object.values(description)).map((value,index) => (
-          <p name={name} key={index} className={`${index === 0 ? 'tile-title' : ''} tile`}>
+      <p className="tile-title tile">
+        {name}
+      </p>
+      {Object.values(description).map((value,index) => (
+          <p key={index} className="tile">
             {value}
           </p>
-        ))
-      }
+      ))}
     </div>
   );
 };
