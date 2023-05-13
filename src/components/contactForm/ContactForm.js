7686aa4d06ea724eaa97793e18729ff75441bc13
Anne-Marie <108076177+Aimes13@users.noreply.github.com>
2023-05-14 01:08:15 +0800

    Update ContactForm.js

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,26 +7,48 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit, 
+  duplicate
 }) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)} />
-        <input 
-          type="tel"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          // regex for phone number
-          pattern="[0-9]{10}" />
-        <input 
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)} />
-        <input type="submit" />
+        <label>
+          {duplicate && <span style={ {color: 'red'} }>&nbsp;&nbsp; This contact already exists</span>}
+        </label>
+        <label>
+          <input 
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Contact Name"
+            aria-label="Contact Name"
+            required />
+        </label>
+        <label>
+          <input 
+            type="tel"
+            name="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            // regex for phone number
+            pattern="[0-9]{10}"
+            placeholder="Contact Number (0123456789)"
+            aria-label="Contact Number"
+            required />
+        </label>
+        <label>
+          <input 
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Contact Email"
+            aria-label="Contact Email"
+            required />
+        </label>
+        <input type="submit" placeholder="Add Contact" aria-label="Add Contact" />
       </form>
     </>
   );
